e0935d23b4d848d9e5787654ed4785cdc13d3ba1
Alina Moskieva <alina.moskieva@flatstack.com>
2019-03-20 07:25:39 +0100

    Add results to show page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   expose :users, -> { UsersQuery.new.all }
   expose :users_from_matview, -> { UsersStatesMatview.all }
+  expose :people, -> { PeopleQuery.new.all }
 
   def statistics
   end
